1f29d88181de4f047ef69755eee645c07798c65e
contactwajeeh <mohammed.wajee@gmail.com>
2020-10-13 19:44:05 -0400

    Building our react app

--- src/config.js
+++ src/config.js
@@ -1,16 +1,17 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
-    REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-    BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    REGION: "us-east-1",
+    BUCKET: "wajee-notes-app-tutorial"
   },
   apiGateway: {
-    REGION: "YOUR_API_GATEWAY_REGION",
-    URL: "YOUR_API_GATEWAY_URL"
+    REGION: "us-east-1",
+    URL: "https://vmqwwb2m2j.execute-api.us-east-1.amazonaws.com/prod"
   },
   cognito: {
-    REGION: "YOUR_COGNITO_REGION",
-    USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-    APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-    IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_LHeT2ehiX",
+    APP_CLIENT_ID: "50pf56qbnqm173ld2q00v945gd",
+    IDENTITY_POOL_ID: "us-east-1:80f1574d-038a-4d37-9c5f-67b1b1d1dde2"
   }
 };
